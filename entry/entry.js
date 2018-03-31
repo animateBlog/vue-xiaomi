d1ec364ab0ad4c6e570afedbd46a6f5dbb48c45a
animateJack <liuweimin@yahoo.com>
2018-03-31 11:33:29 +0800

    user页面主要内容和组件

---
+++ entry/entry.js
@@ -0,0 +1,29 @@
+//npm
+//import Vue from "vue";
+//手动下载
+import Vue from "vue";
+require("./css/base.css");
+import xuser_pannel from "./components/user_pannel.vue";
+import xuser_header from "./components/user_header.vue";
+import xuser_order from "./components/user_order.vue";
+new Vue({
+	//容器作用域
+	el: "#user",
+	//容器内容
+	template: `
+		<div style="height:100%; width:100%" id="user_page">
+			<xuser_header />
+			<xuser_order />
+			<xuser_pannel />
+		</div>
+	`,
+	data: {
+
+	},
+	methods: {},
+	components: {
+		xuser_pannel,
+		xuser_header,
+		xuser_order
+	}
+})
